7d774d544ecea98408b05d4cc104e2b1c433ccf6
Aly Badawy <AlyBadawy@users.noreply.github.com>
2025-01-21 12:21:21 -0500

    Breakdown plugins

--- init.lua
+++ init.lua
@@ -1,11 +1,5 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
@@ -21,50 +15,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  }
-}
-local opts = {}
 
-require("lazy").setup(plugins, opts)
+require("vim-options")
 
-local telescope = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', telescope.find_files, {})
-vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
-local treesitter = require('nvim-treesitter.configs')
-treesitter.setup({
-  ensure_installed = { 
-    'lua',
-    'javascript',
-    'vim',
-    'vimdoc',
-    'query',
-    'markdown',
-    'markdown_inline'
-  },
-  sync_install = false,
-  highlight = { enable = true },
-  indent = { enable = true }
-})
+require("lazy").setup("plugins")
 
 
 
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = { 
+          'lua',
+          'javascript',
+          'vim',
+          'vimdoc',
+          'query',
+          'markdown',
+          'markdown_inline'
+        },
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  }
+}
