32dbf4ca1cb7a4e8e34c1220872803f226747e92
Aly Badawy <AlyBadawy@users.noreply.github.com>
2025-01-21 19:59:41 -0500

    Add LSP

---
+++ lua/plugins/mason-lsp.lua
@@ -0,0 +1,32 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+  lazy = false,
+  opts = {
+    auto_install = true,
+  },
+  config = function()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "clangd",
+        "cmake",
+        "cssls",
+        "tailwindcss",
+        "dockerls",
+        "elmls",
+        "ember",
+        "emmet_ls",
+        "html",
+        "stimulus_ls",
+        "eslint",
+        "ts_ls",
+        "rubocop",
+        "ruby_lsp",
+        "sqlls",
+        "tsp_server",
+        "yamlls"
+      }
+    })
+  end
+}
+
